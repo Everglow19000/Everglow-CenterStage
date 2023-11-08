34a5f8561e09f9178216b80a25b216c02c12b7c9
Omri <omri.r2007@gmail.com>
2023-11-08 21:18:27 +0200

    arrange master and add basic packages

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Modle.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//more imports
+
+@TeleOp(name = "Modle Op-Mode")
+public class OpMode_Modle extends LinearOpMode {
+
+    //make more attributes & functions
+
+    @Override
+    public void runOpMode(){
+        //write here code
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class test {
-    public static void main(String[] arg){
-        System.out.println("hello world");
-    }
-}
