f18a1e6d7ba4498b8c1d2179cb1f39ed3e7541fe
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-01-06 18:02:35 +0200

    odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Modle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode_Modle.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 //more imports
 
@@ -9,9 +11,24 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class OpMode_Modle extends LinearOpMode {
 
     //make more attributes & functions
-
+    final double TicksToCm = 0.0586;
+    DcMotor FR,FL,BR,BL;BNO055IMU imu;
     @Override
     public void runOpMode(){
-        //write here code
+        TestMotor(50, FR);
+        sleep(20000);
+        TestMotor(50, FL);
+        sleep(20000);
+        TestMotor(50, BR);
+        sleep(20000);
+        TestMotor(50, BL);
+    }
+    void TestMotor(double dis, DcMotor motor){
+        double speed = 0.5;
+        double power;
+        while (motor.getCurrentPosition()*TicksToCm < dis ) {
+            power = ((dis - motor.getCurrentPosition()*TicksToCm)/dis)*speed;
+            motor.setPower(power);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -21,8 +21,8 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+    public static final double TICKS_PER_REV = 2000;
+    public static final double MAX_RPM = 624;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -44,9 +44,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 4.8; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in
+    public static double TRACK_WIDTH = 37.5; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -65,18 +65,18 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_VEL = rpmToVelocity(MAX_RPM);
+    public static double MAX_ACCEL = MAX_VEL;
+    public static double MAX_ANG_VEL = MAX_VEL/TRACK_WIDTH;
+    public static double MAX_ANG_ACCEL = MAX_ANG_VEL;
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
      */
     public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
-            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+            RevHubOrientationOnRobot.LogoFacingDirection.FORWARD;
     public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
-            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+            RevHubOrientationOnRobot.UsbFacingDirection.DOWN;
 
 
     public static double encoderTicksToInches(double ticks) {
