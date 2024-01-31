66ee33835ff4474a02feb3e684062b7060aa37d4
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-01-31 21:11:49 +0200

    whatever tf ivri is doing idk
    and the gear ratio change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -45,7 +45,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 4.8; // cm
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1/19.2; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 37.5; // cm
 
     /*
