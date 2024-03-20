a649b1c17258b72c27a4b2c2f2d7f0e7e12f5916
BuilderOfOz <150072067+aviv-nachum@users.noreply.github.com>
2024-03-20 22:23:53 +0200

    super spline test tuning
    seems to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0072;
+    public static double kV = 0.0058;
     public static double kA = 0.0015;
-    public static double kStatic = 0.02;
+    public static double kStatic = 0.1184;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
